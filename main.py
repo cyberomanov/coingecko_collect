d93e5c5206ac2b28c02ad8c8385389471c828318
Daniil Romanov <41644451+cyberomanov@users.noreply.github.com>
2021-06-14 14:56:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,41 +21,41 @@ while True:
     random.shuffle(accounts_list)
 
     for i in range(len(accounts_list)):
-
-        # получаем логин, пароль, прокси для логина
-        login = accounts_list[i]["coin_mail"]
-        passwd = accounts_list[i]["coin_password"]
-        proxy = { 'https': 'http://' + accounts_list[i]["proxy"],
-                  'http': 'http://' + accounts_list[i]["proxy"]
-                }
-
-        # генерируем фейковый юзер-агент
-        user = fake_useragent.UserAgent().random
-        header = {
-            'user-agent': user
-        }
-
-        # создаём сессию и подключаем прокси
-        session = requests.Session()
-        session.proxies = proxy
-
-        # выполняем логин
-        if login_coingecko(session=session, login=login, passwd=passwd, header=header):
-            sleep(seconds_to_sleep)
-            result_of = get_balance_and_token(session=session, header=header)
-            if result_of != False:
+        try:
+            
+            # получаем логин, пароль, прокси для логина
+            login = accounts_list[i]["coin_mail"]
+            passwd = accounts_list[i]["coin_password"]
+            proxy = { 'https': 'http://' + accounts_list[i]["proxy"],
+                      'http': 'http://' + accounts_list[i]["proxy"]
+                    }
+        
+            # генерируем фейковый юзер-агент
+            user = fake_useragent.UserAgent().random
+            header = {
+                'user-agent': user
+            }
+        
+            # создаём сессию и подключаем прокси
+            session = requests.Session()
+            session.proxies = proxy
+        
+            # выполняем логин
+            if login_coingecko(session=session, login=login, passwd=passwd, header=header):
                 sleep(seconds_to_sleep)
-                collect_candies(result_of, session=session, header=header)
+                result_of = get_balance_and_token(session=session, header=header)
+                if result_of != False:
+                    sleep(seconds_to_sleep)
+                    collect_candies(result_of, session=session, header=header)
+                else:
+                    print(f'we have to sleep, cant redeem candies.')
             else:
-                print(f'we have to sleep, cant redeem candies.')
-        else:
+                print(f'something went wrong.')
+        
+            # пауза между аккаунтами от 3 до 6 минут
+            sleep(seconds_to_sleep*25)
+        except:
             print(f'something went wrong.')
-
-        # пауза между аккаунтами от 3 до 6 минут
-        sleep(seconds_to_sleep*25)
 
     # итерация прошла, спим пару часов
     sleep(50000)
-
-
-
