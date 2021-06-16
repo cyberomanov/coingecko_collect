6b98f8a6b8062217e3023b89a9ce76b16acaad19
Daniil Romanov <41644451+cyberomanov@users.noreply.github.com>
2021-06-16 17:35:43 +0300

    Update func.py

--- func.py
+++ func.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 import datetime
-import colorama as color
 import random
 import time
+
+
 
 
 def sleep(seconds):
@@ -10,17 +11,20 @@ def sleep(seconds):
     time.sleep(seconds-difference)
 
 
+
 def beautiful_print(text):
     length = 68
     if len(text) % 2:
         length += 1
     print()
-    print(color.Fore.LIGHTMAGENTA_EX + ('{0: ^' + str(length) + '}').format(text))
-    print(color.Fore.WHITE)
+    print(('{0: ^' + str(length) + '}').format(text))
+    print()
+
 
 
 def get_time():
     return datetime.datetime.now().strftime("%H:%M:%S")
+
 
 
 def login_coingecko(session, login, passwd, header):
@@ -51,19 +55,80 @@ def login_coingecko(session, login, passwd, header):
         return False
 
 
+
 def get_balance_and_token(session, header):
     # отправляем запрос на баланс токенов
-    balance_request = session.get('https://www.coingecko.com/account/candy?locale=en', headers=header)
-    soup = BeautifulSoup(balance_request.text, 'lxml')
-
+    response = session.get('https://www.coingecko.com/account/candy?locale=en', headers=header)
+    soup = BeautifulSoup(response.text, 'lxml')
     balance = soup.find('div', {'data-target': 'points.balance'}).text
     print(f'balance: {balance} candies.', end=' ')
 
+    return parse_token(response)
+
+
+
+def parse_token(response):
     try:
-        csrf_token = soup.find('input', {'name': 'authenticity_token'})['value']
-        return csrf_token
+        soup = BeautifulSoup(response.text, 'lxml')
+        token = soup.find('input', {'name': 'authenticity_token'})['value']
+        return token
     except:
         return False
+
+
+
+def get_reward(link, session, header):
+
+    # получаем токен
+    response = session.get(link, headers=header)
+    token = parse_token(response)
+
+    # если токен получен
+    if token != False:
+
+        # получем действие
+        soup = BeautifulSoup(response.text, 'lxml')
+        action = soup.findAll('form', {'class': 'button_to'})[1]['action']
+
+        # получаем название предмета
+        title = soup.find('div', {'class': 'text-lg-2xl text-xl pl-1 font-weight-bold'})
+        data = {
+            'authenticity_token': token,
+        }
+
+        # отправляем запрос покупки реварда
+        buy_response = session.post(url="https://www.coingecko.com/"+action, data=data,
+                                     headers=header)
+        if buy_response.ok:
+            print(f'successfully bought.', end=' ')
+            return title.text
+        else:
+            print(f'GG.')
+            return False
+    else:
+        print(f'GG.')
+        return False
+
+
+
+def get_promo(title, session, header):
+    response = session.get("https://www.coingecko.com/account/my-rewards?locale=en", headers=header)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    # получаем список полученных наград
+    rewards = soup.findAll('div', {'class': 'ml-3 mr-md-3 ml-lg-4 mb-3 mr-lg-1 voucher-card-section'})
+    for reward in rewards:
+
+        # ищем ссылку на награду по названию покупки
+        if title in reward.contents[3].text:
+
+            # достаём промо
+            href = reward.contents[1].contents[1]['href']
+            response = session.get("https://www.coingecko.com/" + href, headers=header)
+            soup = BeautifulSoup(response.text, 'lxml')
+            promo = soup.find('input', {'class': 'form-control font-semibold'})['value']
+            print(f'promo: {promo}.')
+            break
 
 
 
@@ -72,7 +137,7 @@ def collect_candies(csrf_token, session, header):
     data = {
         'authenticity_token' : csrf_token
     }
-    collect_request = session.post('https://www.coingecko.com/account/candy/daily_check_in?locale=en', headers=header,data=data)
+    collect_request = session.post('https://www.coingecko.com/account/candy/daily_check_in?locale=en', headers=header, data=data)
     if collect_request.ok:
         print(f'successfully redeemed.', end=' ')
         # получаю новый баланс
